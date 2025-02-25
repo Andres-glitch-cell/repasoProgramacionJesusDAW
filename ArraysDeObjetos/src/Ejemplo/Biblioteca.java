1252990f364202e02ec2f6ff05c97f3c1c155c12
AndresFernandezSalaud <afernandezs.iesjc@gmail.com>
2025-02-25 19:10:58 +0100

    25/02/2025 Programación Orientada a Objetos

---
+++ ArraysDeObjetos/src/Ejemplo/Biblioteca.java
@@ -0,0 +1,13 @@
+package Ejemplo;
+
+public class Biblioteca {
+    private String libro;
+
+    public Biblioteca(String NombreLibro) {
+        libro = NombreLibro;
+    }
+
+    public String getLibro() {
+        return libro;
+    }
+}
